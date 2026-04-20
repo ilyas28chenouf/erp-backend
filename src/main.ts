2c635b4d0825729655efc5d3e21b3b23744653dc
ilyas28chenouf <ilyaschenouf28@gmail.com>
2026-04-20 12:10:16 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,24 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
+  app.enableCors({
+      origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
   app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
   );
+  await app.listen(3000, '0.0.0.0');
+
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector)),
   );
